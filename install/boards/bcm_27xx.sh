db3107373ce6230d4969426f8bb5d1c918f00ee6
goasChris <kristiansen@goas.no>
2024-10-30 10:34:18 +0000

    Disable BT

--- install/boards/bcm_27xx.sh
+++ install/boards/bcm_27xx.sh
@@ -60,6 +60,7 @@ for STRING in \
     "gpio=11,24,25=op,pu,dh" \
     "gpio=37=op,pd,dl" \
     "dtoverlay=dwc2,dr_mode=otg" \
+    "dtoverlay=disable-bt" \
     ; do \
     sed -i "$line_number r /dev/stdin" $CONFIG_FILE <<< "$STRING"
 done
