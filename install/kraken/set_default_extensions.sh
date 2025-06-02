abe5fe3a70767d5a81eea39a2f13025bc1333587
goasChris <kristiansen@goas.no>
2025-06-02 08:03:12 +0100

    MajorTom: Temporarily remove
    Major Tom is currently unavaliable, past one hundred thousand miles

--- install/kraken/set_default_extensions.sh
+++ install/kraken/set_default_extensions.sh
@@ -3,7 +3,6 @@
 # Extensions data fetch, use for Extensions that are synced by some online source
 
 COCKPIT_RELEASE_URL="https://api.github.com/repos/goasChris/cockpit/releases"
-MAJOR_TOM_RELEASE_URL="https://blueos.cloud/major_tom/install"
 
 alias curl="curl --retry 6 --max-time 15 --retry-all-errors --retry-delay 20 --connect-timeout 60"
 
@@ -17,31 +16,11 @@ else
   exit 1
 fi
 
-major_tom_install_data=$(curl -fsSL --connect-timeout 60 --max-time 60 --retry 5 --retry-delay 20 $MAJOR_TOM_RELEASE_URL)
-
-if [[ $major_tom_install_data =~  \"docker\":\ *\"([^\"]+)\" ]]; then
-  major_tom_docker="${BASH_REMATCH[1]}"
-else
-  echo "Could not find the latest release docker value of Major Tom."
-  exit 1
-fi
-
-if [[ $major_tom_install_data =~  \"tag\":\ *\"([^\"]+)\" ]]; then
-  major_tom_tag="${BASH_REMATCH[1]}"
-else
-  echo "Could not find the latest release tag value of Major Tom."
-  exit 1
-fi
-
-echo "Using major tom: $major_tom_docker:$major_tom_tag"
-
 # Images pulling
 
 BLUEROBOTICS_COCKPIT_EXT="goaschris/cockpit:$cockpit_tag_name"
-BLUEROBOTICS_MAJOR_TOM_EXT="$major_tom_docker:$major_tom_tag"
 
 docker pull $BLUEROBOTICS_COCKPIT_EXT
-docker pull $BLUEROBOTICS_MAJOR_TOM_EXT
 
 # Settings creation
 
@@ -69,8 +48,7 @@ cat > "${SETTINGS_BASE_DIR}/settings-2.json" <<EOF
       "permissions": "{\"ExposedPorts\":{\"8000/tcp\":{}},\"HostConfig\":{\"PortBindings\":{\"8000/tcp\":[{\"HostPort\":\"\"}]}}}",
       "tag": "$cockpit_tag_name",
       "user_permissions": ""
-    },
-    $major_tom_install_data
+    }
   ],
   "manifests": []
 }
