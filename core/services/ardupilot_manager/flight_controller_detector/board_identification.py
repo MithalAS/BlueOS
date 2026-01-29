7f1e4586c8ef372fcc8b9d01c1ee2a82824ed2c0
AdneHK96 <adne@remorarobotics.no>
2026-01-29 17:39:01 +0100

    plus test

--- core/services/ardupilot_manager/flight_controller_detector/board_identification.py
+++ core/services/ardupilot_manager/flight_controller_detector/board_identification.py
@@ -26,6 +26,8 @@ identifiers: List[SerialBoardIdentifier] = [
     SerialBoardIdentifier(attribute=SerialAttr.product, id_value="FMU v6X.x", platform=Platform.Pixhawk6X),
     SerialBoardIdentifier(attribute=SerialAttr.product, id_value="FMU v6C.x", platform=Platform.Pixhawk6C),
     SerialBoardIdentifier(attribute=SerialAttr.product, id_value="CubeOrange", platform=Platform.CubeOrange),
+    SerialBoardIdentifier(attribute=SerialAttr.product, id_value="CubeOrange+", platform=Platform.CubeOrange),
+    SerialBoardIdentifier(attribute=SerialAttr.product, id_value="CubeOrangePlus", platform=Platform.CubeOrange),
     SerialBoardIdentifier(attribute=SerialAttr.manufacturer, id_value="ArduPilot", platform=Platform.GenericSerial),
     SerialBoardIdentifier(attribute=SerialAttr.manufacturer, id_value="Arduino", platform=Platform.GenericSerial),
     SerialBoardIdentifier(attribute=SerialAttr.manufacturer, id_value="3D Robotics", platform=Platform.GenericSerial),
